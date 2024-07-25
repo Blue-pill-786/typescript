afaae7d9ec2cfcb6cc726961ca8e156632f3f012
Blue-pill-786 <113605539+Blue-pill-786@users.noreply.github.com>
2024-07-25 09:14:40 +0530

    typescript

--- helloworld/dest/index.js
+++ helloworld/dest/index.js
@@ -1,5 +1,73 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
+var _a;
+let sales = 123456789;
+let course = "Typescript";
+let is_published = true;
+let level;
+level = 1;
+level = 'a';
+let numbers = [1, 2, 3, 4, 5, 6];
+numbers.forEach(n => n.toFixed());
+let user = [1, 'Mosh'];
+user.push(1);
+user.push(2);
+console.log(user);
+var Size;
+(function (Size) {
+    Size["small"] = "s";
+    Size["medium"] = "m";
+    Size["large"] = "l ";
+})(Size || (Size = {}));
+;
+let mySize = Size.medium;
+console.log(mySize);
+function calculateTax(income) {
+    if (income < 50000)
+        return income * 1.2;
+    return income * 1.3;
+}
+let employee = {
+    id: 1,
+    name: 'Mosh',
+    retire: (date) => {
+        console.log(date);
+    }
+};
+let a = {
+    id: 1,
+    name: 'Mosh',
+    retire: (date) => {
+        console.log(date);
+    }
+};
+function kgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+kgToLbs(10);
+kgToLbs('10kgs');
+let textBox = {
+    drag: () => {
+        console.log('dragging');
+    },
+    resize: () => {
+        console.log('resizing');
+    }
+};
+let quantity = 50;
+let unit = 'cm';
+function greet(name) {
+    if (name)
+        console.log("Hello! " + name.toUpperCase());
+    else
+        console.log('Hello');
+}
+greet(null);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(0);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
 //# sourceMappingURL=index.js.map
--- helloworld/src/index.ts
+++ helloworld/src/index.ts
@@ -1,3 +1,126 @@
-let age : number =20;
-if(age< 50)
-    age+=10
+let sales = 123_456_789;
+let course = "Typescript";
+let is_published = true;
+let level;
+level= 1;
+level ='a';
+
+let numbers= [1,2,3,4,5,6];
+
+numbers.forEach(n=>n.toFixed())
+
+let user: [number, string] = [1, 'Mosh']
+user.push(1);
+user.push(2);
+console.log(user)
+enum Size {small = 's', medium='m', large='l '};
+let mySize : Size= Size.medium;
+console.log(mySize);
+function calculateTax(income: number){
+   
+    if(income<50_000)
+        return income *1.2
+    return income * 1.3;
+}
+
+
+let employee:{
+    readonly id: number,
+    name: string,
+    retire:(date:Date)=>void
+} = {
+    id:1, 
+    name:'Mosh',
+    retire: (date:Date)=>{
+        console.log(date)
+    }
+};
+
+type Employee = {
+    readonly id: number,
+    name: string,
+    retire:(date:Date)=>void
+}
+
+let a: Employee ={
+    id:1,
+    name:'Mosh',
+    retire: (date:Date)=>{
+        console.log(date)
+        }
+}
+
+
+function kgToLbs(weight:number|string): number{
+   if (typeof weight ==='number')
+    return weight * 2.2;
+else
+return parseInt(weight) * 2.2;
+}
+
+kgToLbs(10);
+kgToLbs('10kgs');
+
+//type intersection in action
+type draggable = {
+    drag: () => void;
+};
+type resizeable ={
+    resize: ()=>void
+}
+type UIWidget = draggable & resizeable;
+
+let textBox: UIWidget = {
+    drag: () => {
+        console.log('dragging')
+    },  
+    resize:()=>{
+        console.log('resizing')
+    }
+} ;
+
+//literal types
+//literal (exact, specific)
+type Quantity = 50 | 100 | 200;
+
+let quantity:Quantity =50;
+
+type Metric = 'cm' | 'inch'
+
+let unit: Metric = 'cm' 
+
+//nullable types
+
+function greet(name:string | null | undefined){
+    if (name)
+    console.log("Hello! "+name.toUpperCase());
+    else
+    console.log('Hello');
+}
+
+greet(null)
+
+//optional chaining
+
+type Customer ={
+    birthday: Date
+};
+
+function getCustomer(id:number): Customer|null|undefined{
+    return id===0 ? null :{ birthday: new Date()}
+}
+
+let customer = getCustomer(0);
+// if(customer !== null && customer !== undefined)
+//instead we can use optional property access operator
+
+console.log(customer?.birthday?.getFullYear())
+
+//optional element access operator
+//customers?.[0]
+
+//optional call
+let log: any = null
+
+log?.('a')
+
